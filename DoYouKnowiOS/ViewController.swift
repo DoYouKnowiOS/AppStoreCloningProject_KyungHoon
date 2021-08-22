76854f10c363370ea89cf01358441b692ea27c9f
kimkyunghun3 <kkh3@cau.ac.kr>
2021-08-22 14:03:15 +0900

    Initial Commit

---
+++ DoYouKnowiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoYouKnowiOS
+//
+//  Created by KimKyungHoon on 2021/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
